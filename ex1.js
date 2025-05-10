8efde0b80b0c091c8ca7f89f9268e1b9f9e5e221
vhbt34 <victorhugo.88.vt@gmail.com>
2025-05-10 01:48:55 -0300

    remove comentários desnecessários no código do livro

--- ex1.js
+++ ex1.js
@@ -1,10 +1,8 @@
-// Objeto representando um livro
 const livro = {
     titulo: "O Senhor dos Anéis",
     autor: "J.R.R. Tolkien",
     numeroDePaginas: 1216
   };
   
-  // Emissão da variável no console
   console.log(livro);
   
